3bdf53fdc07f11a4ac18dc787c10edf661438e88
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-23 18:00:53 +0530

    load guide details on tables

--- controller/GuideController.js
+++ controller/GuideController.js
@@ -11,74 +11,74 @@ const regExAddress = /^[0-9A-Z a-z,/:]{4,50}$/;
 const regExDayValue = /^[0-9]{1,10}(.)[0-9]{2}$/;
 
 // Guide Name Input validation
-$("#guide_name").keydown(function (event) {
-  const name = $('#guide_name').val();
-  if (regExGuideName.test(name)) {
-    $("#guide_name").css('border', '2px solid #134eed');
-    if (event.key == "Enter") {
-      $("#guidAddress").focus();
-    }
-  } else {
-    $("#guide_name").css('border', '2px solid red');
-    console.log("Invaid name Check again!");
-  }
-});
+// $("#guide_name").keydown(function (event) {
+//   const name = $('#guide_name').val();
+//   if (regExGuideName.test(name)) {
+//     $("#guide_name").css('border', '2px solid #134eed');
+//     if (event.key == "Enter") {
+//       $("#guidAddress").focus();
+//     }
+//   } else {
+//     $("#guide_name").css('border', '2px solid red');
+//     console.log("Invaid name Check again!");
+//   }
+// });
 
-// Guide Address Input validation
-$("#guidAddress").keydown(function (event) {
-  const address = $('#guidAddress').val();
-  if (regExAddress.test(address)) {
-    $("#guidAddress").css('border', '2px solid #134eed');
-    if (event.key == "Enter") {
-      $("#age").focus();
-    }
-  } else {
-    $("#guidAddress").css('border', '2px solid red');
-    console.log("Invaid Address Check again!");
-  }
-});
+// // Guide Address Input validation
+// $("#guidAddress").keydown(function (event) {
+//   const address = $('#guidAddress').val();
+//   if (regExAddress.test(address)) {
+//     $("#guidAddress").css('border', '2px solid #134eed');
+//     if (event.key == "Enter") {
+//       $("#age").focus();
+//     }
+//   } else {
+//     $("#guidAddress").css('border', '2px solid red');
+//     console.log("Invaid Address Check again!");
+//   }
+// });
 
-// Guide Age Input validation
-$("#age").keydown(function (event) {
-  const age = $('#age').val();
-  if (regExAge.test(age)) {
-    $("#age").css('border', '2px solid #134eed');
-    if (event.key == "Enter") {
-      $("#gender").focus();
-    }
-  } else {
-    $("#age").css('border', '2px solid red');
-    console.log("Invaid Age Check again!");
-  }
-});
+// // Guide Age Input validation
+// $("#age").keydown(function (event) {
+//   const age = $('#age').val();
+//   if (regExAge.test(age)) {
+//     $("#age").css('border', '2px solid #134eed');
+//     if (event.key == "Enter") {
+//       $("#gender").focus();
+//     }
+//   } else {
+//     $("#age").css('border', '2px solid red');
+//     console.log("Invaid Age Check again!");
+//   }
+// });
 
-// Guide Contact Input validation
-$("#contact").keydown(function (event) {
-  const contact = $('#contact').val();
-  if (regExContact.test(contact)) {
-    $("#contact").css('border', '2px solid #134eed');
-    if (event.key == "Enter") {
-      $("#experiences").focus();
-    }
-  } else {
-    $("#contact").css('border', '2px solid red');
-    console.log("Invaid Contact No: Check again!");
-  }
-});
+// // Guide Contact Input validation
+// $("#contact").keydown(function (event) {
+//   const contact = $('#contact').val();
+//   if (regExContact.test(contact)) {
+//     $("#contact").css('border', '2px solid #134eed');
+//     if (event.key == "Enter") {
+//       $("#experiences").focus();
+//     }
+//   } else {
+//     $("#contact").css('border', '2px solid red');
+//     console.log("Invaid Contact No: Check again!");
+//   }
+// });
 
-// Guide DayValue Input validation
-$("#day_value").keydown(function (event) {
-  const dayValue = $('#day_value').val();
-  if (regExDayValue.test(dayValue)) {
-    $("#contact").css('border', '2px solid #134eed');
-    if (event.key == "Enter") {
-      $("#remark").focus();
-    }
-  } else {
-    $("#contact").css('border', '2px solid red');
-    console.log("Invaid Contact No: Check again!");
-  }
-});
+// // Guide DayValue Input validation
+// $("#day_value").keydown(function (event) {
+//   const dayValue = $('#day_value').val();
+//   if (regExDayValue.test(dayValue)) {
+//     $("#contact").css('border', '2px solid #134eed');
+//     if (event.key == "Enter") {
+//       $("#remark").focus();
+//     }
+//   } else {
+//     $("#contact").css('border', '2px solid red');
+//     console.log("Invaid Contact No: Check again!");
+//   }
+// });
 
 //Key Events
 $('#guide-save-btn').click(function (e) {
@@ -109,15 +109,17 @@ $('#guide-save-btn').click(function (e) {
     policyId: policyId
   };
 
+  // console.log("guideobject :",guideObj);
+
   // Create Post Request
   $.ajax({
     url: baseURL + "save",
-    method: "POST",
+    method: "post",
     data: JSON.stringify(guideObj),
     contentType: "application/json",
     dataType: "json",
     success: function (response) {
-      if (response.code == 200){
+      if (response.code == 200) {
         alert("response ok");
         getAllGuideDetails();
         clearFields();
@@ -135,31 +137,35 @@ function getAllGuideDetails() {
   $.ajax({
     url: baseURL + "getAll",
     method: "GET",
-    contentType: "application/json",
-    dataType: "json",
     success: function (response) {
-      console.log(response);
-      // $("#Guide-Details-table tbody").empty();
-      // for (var responseKey of response.data) {
-      //   let rawData = `<tr>
-      //           <td> ${responseKey.driverId}</td>
-      //           <td>${responseKey.driverName}</td>
-      //           <td> ${responseKey.driverAddress}</td>
-      //           <td> ${responseKey.driverAge}</td>
-      //           <td> ${responseKey.driverContact}</td>
-      //           <td>
-      //           <button type="button" id="btnEditDriver" 
-      //           class="btn btn-primary btn-sm px-3" data-ripple-color="dark">
-      //           <i class="bi bi-pencil-square"></i>
-      //           </button>
-      //           <button type="button" id="btnEditDriver" 
-      //           class="btn btn-danger btn-sm px-3" data-ripple-color="dark">
-      //           <i class="bi bi-trash3-fill"></i>
-      //           </button>
-      //           </td>
-      //           </tr>`;
-      //   $("#Guide-Details-table tbody").append(rawData);
-      // }
+      $("#Guide-Details-table tbody").empty();
+      response.forEach(element => {
+        let rawData = `<tr>
+                <td> ${element.data.guidId}</td>
+                <td>${element.data.guidName}</td>
+                <td> ${element.data.address}</td>
+                <td> ${element.data.age}</td>
+                <td> ${element.data.gender}</td>
+                <td> ${element.data.contact}</td>
+                <td> ${element.data.experience}</td>
+                <td> ${element.data.dayValue}</td>
+                <td> ${element.data.policyId}</td>
+                <td> ${element.data.remarks}</td>
+                <td>
+                <button type="button" id="btn-delete-guide" 
+                class="btn btn-danger btn-sm px-3" data-ripple-color="dark">
+                <i class="bi bi-trash3-fill"></i>
+                </button>
+                </td>
+                <td>
+                <button type="button" id="btn-update-guide" 
+                class="btn btn-primary btn-sm px-3" data-ripple-color="dark">
+                <i class="bi bi-pencil-square"></i>
+                </button>
+                </td>
+                </tr>`;
+        $("#Guide-Details-table tbody").append(rawData);
+      });
     },
     error: function (error) {
       var jsObject = JSON.parse(error.responseText);
@@ -168,8 +174,9 @@ function getAllGuideDetails() {
   });
 }
 
+
 //clear input fields
-function clearFields(){
+function clearFields() {
   $('#guid_Id').val("");
   $('#guide_name').val("");
   $('#guidAddress').val("");
