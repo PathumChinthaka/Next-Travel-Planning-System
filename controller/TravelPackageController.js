cb64e54c93344197600011275dc8f3c13175f534
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-27 21:19:00 +0530

    travel packages crud api tested

--- controller/TravelPackageController.js
+++ controller/TravelPackageController.js
@@ -35,6 +35,8 @@ $("#package-save-btn").click(function (e) {
     success: function (response) {
       if (response.code == 200) {
         alert(response.message);
+        getAllPackageDetails();
+        clearPackageInputs();
       }
     },
     error: function (xhr, status, error) {
@@ -74,3 +76,115 @@ function getAllPackageDetails() {
 
 //get all packages globally called
 getAllPackageDetails();
+
+//update package details event
+$("#package-update-btn").click(function (e) {
+  const packageId = $('#package_id').val();
+  const packageName = $('#package_Name').val();
+  const packagedescription = $('#package_description').val();
+  const packageCategory = $('#package_Category').val();
+  const dayCount = $('#day_Count').val();
+  const travelAreaId = $('#travel_area_id').val();
+  const nightCount = $('#night_Count').val();
+  const petsAllow = $('#pets_Allow').val();
+  const packageVideoUrl = $('#package_video_url').val();
+  const roomType = $('#room_type').val();
+
+  const packageDetails = {
+    packageId: packageId,
+    packageName: packageName,
+    packageDescription: packagedescription,
+    packageCategory: packageCategory,
+    dayCount: dayCount,
+    travelAreas: travelAreaId,
+    nightCount: nightCount,
+    allowPets: petsAllow,
+    roomTypes: roomType,
+    travelPackageVideoUrl: packageVideoUrl
+  }
+
+  $.ajax({
+    url: baseURL + "package/update",
+    method: "put",
+    data: JSON.stringify(packageDetails),
+    contentType: "application/json",
+    dataType: "json",
+    success: function (response) {
+      if (response.code == 200) {
+        alert(response.message);
+        getAllPackageDetails();
+        clearPackageInputs();
+      }
+    },
+    error: function (xhr, status, error) {
+      alert("An error occurred: " + error);
+    }
+  });
+});
+
+//delete package data event
+$("#package-delete-btn").click(function (e) {
+  const packageId = $('#package_id').val();
+  if(packageId==""){
+    alert("Package Id is Empty");
+    return;
+  }else{
+    $.ajax({
+      url: baseURL + "package/" + packageId,
+      method: "delete",
+      dataType: "json",
+      success: function (response) {
+        alert(response.message);
+      },
+      error: function (xhr, status, error) {
+        alert("Package Deleted Succesfully");
+        getAllPackageDetails();
+        clearPackageInputs();
+      }
+    });
+  }
+});
+
+//get selected package table raw
+function getSelectedPackageRaw(){
+  $('#package-tbody').on('click', 'tr', (event) => {
+    const packageId = $(event.target).closest('tr').find('td').eq(0).text();
+    const packageName = $(event.target).closest('tr').find('td').eq(1).text();
+    const packageDescription = $(event.target).closest('tr').find('td').eq(2).text();
+    const packageCategory = $(event.target).closest('tr').find('td').eq(3).text();
+    const dayCount = $(event.target).closest('tr').find('td').eq(4).text();
+    const travelAreas = $(event.target).closest('tr').find('td').eq(6).text();
+    const nightCount = $(event.target).closest('tr').find('td').eq(5).text();
+    const allowPets = $(event.target).closest('tr').find('td').eq(7).text();
+    const packageVideoUrl = $(event.target).closest('tr').find('td').eq(9).text();
+    const roomTypes = $(event.target).closest('tr').find('td').eq(8).text();
+
+    $('#package_id').val(packageId.trim());
+    $('#package_Name').val(packageName.trim());
+    $('#package_description').val(packageDescription.trim());
+    $('#package_Category').val(packageCategory.trim());
+    $('#day_Count').val(dayCount.trim());
+    $('#travel_area_id').val(travelAreas.trim());
+    $('#night_Count').val(nightCount.trim());
+    $('#pets_Allow').val(allowPets.trim());
+    $('#package_video_url').val(packageVideoUrl.trim());
+    $('#room_type').val(roomTypes.trim());
+  });
+}
+
+//get selected table raw
+getSelectedPackageRaw();
+
+//clear package input fields
+function clearPackageInputs(){
+  $('#package_id').val("");
+  $('#package_Name').val("");
+  $('#package_description').val("");
+  $('#package_Category').val("");
+  $('#day_Count').val("");
+  $('#travel_area_id').val("");
+  $('#night_Count').val("");
+  $('#pets_Allow').val("");
+  $('#package_video_url').val("");
+  $('#room_type').val("");
+}
