77af6286e37cf9a51915f2936da513bbedd65796
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-10-29 16:32:25 +0530

    hotel delete req

--- controller/HotelDetailsController.js
+++ controller/HotelDetailsController.js
@@ -5,7 +5,7 @@
 
 //create hotel details object
 function hotelDetails(){
-  
+
   const hotelId = $('#hotel_Id').val();
   const hotelName = $('#hotel_Name').val();
   const hotelCategory = $('#hotel_Category').val();
@@ -46,6 +46,7 @@ function hotelDetails(){
   return hotelDetails;
 }
 
+//hotel details save event
 $("#hotel-save-btn").click(function (e) {
 
   //get returned hotel Details object
@@ -67,4 +68,80 @@ $("#hotel-save-btn").click(function (e) {
       alert("An error occurred: " + error);
     }
   });
+});
+
+//get all hotel details
+function getAllHotelDetails(){
+  $.ajax({
+    url: baseURL + "hotel/getAll",
+    method: "GET",
+    success: function (response) {
+      $("#hotel-details-tbl tbody").empty();
+      response.forEach(element => {
+        let rawData = `<tr>
+                <td class="d-none"> ${element.data.}</td>
+                <td>${element.data.}</td>
+                <td> ${element.data.}</td>
+                <td> ${element.data.}</td>
+                <td> ${element.data.}</td>
+                <td> ${element.data.}</td>
+                <td> ${element.data.}</td>
+                <td> ${element.data.}</td>
+                <td> ${element.data.}</td>
+                <td> ${element.data.}</td>
+                </tr>`;
+        $("#Guide-Details-table tbody").append(rawData);
+      });
+    },
+    error: function (xhr, status, error) {
+      alert("An error occurred: " + error);
+    }
+  });
+}
+
+$("#hotel-update-btn").click(function (e) {
+
+    //get returned hotel Details object
+  const hotelDetailsObj=hotelDetails();
+
+  // Create Put Request
+  $.ajax({
+    url: baseURL + "hotel/update",
+    method: "put",
+    data: JSON.stringify(hotelDetailsObj),
+    contentType: "application/json",
+    dataType: "json",
+    success: function (response) {
+      if (response.code == 200) {
+        alert(response.message);
+      };
+    },
+    error: function (xhr, status, error) {
+      console.error(error);
+      alert("An error occurred: " + error);
+    }
+  });
+});
+
+//delete hotel details event
+$("#hotel-delete-btn").click(function (e) {
+  const hotelId = $('#hotel_Id').val();
+  const choice = confirm("Do you want to delete this Data ?");
+  if(hotelId==""){
+    alert("Hotel Id is Empty");
+    return;
+  }else if(choice == true){
+    $.ajax({
+      url: baseURL + "hotel/" + hotelId,
+      method: "delete",
+      dataType: "json",
+      success: function (response) {
+        alert(response.message);
+      },
+      error: function (xhr, status, error) {
+        alert("Package Deleted Succesfully");
+      }
+    });
+  }else{
+  }
 });
